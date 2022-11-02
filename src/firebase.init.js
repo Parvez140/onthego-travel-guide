520f5bdc4ca743265f1ef7e06a1709cded5f618a
Parvez140 <mohiuddinparvez1@gmail.com>
2022-11-02 21:59:27 +0600

    handle firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgAPchCQLvCi0O43ZhV7dhA99SSeJoaCA",
-  authDomain: "onthego-traval-guide.firebaseapp.com",
-  projectId: "onthego-traval-guide",
-  storageBucket: "onthego-traval-guide.appspot.com",
-  messagingSenderId: "102785754848",
-  appId: "1:102785754848:web:8404ceed41038b33b99285",
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
